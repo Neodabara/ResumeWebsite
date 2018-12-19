beb3e4e80f203f0813e4ce443f291048c94d1e1c
Neodabara <31937617+Neodabara@users.noreply.github.com>
2018-12-18 23:23:03 -0500

    Add files via upload

---
+++ Migrations/20181215224352_CourseID.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ResumeWebsite.Migrations
+{
+    public partial class CourseID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseNumber",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseID",
+                table: "Courses",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseID",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseNumber",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
