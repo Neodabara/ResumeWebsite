4441a656bd193d6ded72ebd27d3749853c633a57
Neodabara <31937617+Neodabara@users.noreply.github.com>
2018-12-18 23:21:27 -0500

    Add files via upload

---
+++ Data/ResumeWebsiteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResumeWebsite.Models
+{
+    public class ResumeWebsiteContext : DbContext
+    {
+        public ResumeWebsiteContext (DbContextOptions<ResumeWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ResumeWebsite.Models.Courses> Courses { get; set; }
+    }
+}
