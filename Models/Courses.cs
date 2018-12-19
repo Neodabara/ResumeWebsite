be4a3e4c95d853bdfb2b70ea9def145b5d956f9d
Neodabara <31937617+Neodabara@users.noreply.github.com>
2018-12-18 23:23:14 -0500

    Add files via upload

---
+++ Models/Courses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeWebsite.Models
+{
+    public class Courses
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Course Name")] //sets the display name 
+        [Required] //makes it a required field
+        public string CourseName { get; set; }
+
+        [Display(Name = "Course Number")] //sets the display name
+        [Range(100, 999)] //sets the range of the acceptable numbers to 100 - 999
+        [Required] // makes it a required field
+        public int CourseNumber { get; set; }
+
+       [Display(Name = "Course ID")] //sets the display name
+       [StringLength(2)] // sets the acceptable length for the string entered to no more than 2 (IE CS is ok, C is ok, CSS is not)
+       [Required] // makes it a required field
+        public string CourseID { get; set; }
+    }
+}
